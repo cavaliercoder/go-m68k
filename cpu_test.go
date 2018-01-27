2b275306df6e9a590536c776096823a94a13fb36
Ryan Armstrong <ryan@cavaliercoder.com>
2018-01-27 15:30:08 +0800

    Moved test helpers to new package

--- cpu_test.go
+++ cpu_test.go
@@ -2,113 +2,14 @@ package m68k_test
 
 import (
 	"io"
-	"os"
 	"testing"
 
-	"github.com/cavaliercoder/go-m68k"
-	"github.com/cavaliercoder/go-m68k/srec"
+	"github.com/cavaliercoder/go-m68k/m68ktest"
 )
-
-type testLogWriter struct {
-	t *testing.T
-}
-
-func (t *testLogWriter) Write(p []byte) (n int, err error) {
-	if t.t == nil {
-		return
-	}
-	t.t.Logf("%s", p)
-	n = len(p)
-	return
-}
-
-func testProc(t *testing.T) *m68k.Processor {
-	p := &m68k.Processor{
-		TraceWriter: &testLogWriter{t},
-	}
-	p.Reset()
-	return p
-}
-
-func loadFile(t *testing.T, path string) *m68k.Processor {
-	p := testProc(t)
-	f, err := os.Open("./testdata/" + path)
-	if err != nil {
-		panic(err)
-	}
-	defer f.Close()
-	recs, err := srec.Read(f)
-	if err != nil {
-		panic(err)
-	}
-	for _, rec := range recs {
-		if rec.IsData() {
-			if _, err := p.M.Write(rec.Address(), rec.Data()); err != nil {
-				panic(err)
-			}
-		}
-	}
-	return p
-}
-
-func loadBytes(t *testing.T, b []byte) *m68k.Processor {
-	p := testProc(t)
-	if _, err := p.M.Write(0x1000, b); err != nil {
-		panic(err)
-	}
-	return p
-}
-
-func assertRun(t *testing.T, p *m68k.Processor) {
-	err := p.Jump(0x1000)
-	if err != nil {
-		t.Errorf("error resetting program counter: %v", err)
-	}
-	err = p.Run()
-	if err == io.EOF {
-		return
-	}
-	if err != nil {
-		t.Errorf("error running program: %v", err)
-		m68k.Dump(os.Stdout, p.M)
-	}
-}
-
-func assertDataRegister(t *testing.T, p *m68k.Processor, r int, v uint32) {
-	if p.D[r] != v {
-		t.Errorf("expected data value 0x%08X in D%d, got 0x%08X", v, r, p.D[r])
-	}
-}
-
-func assertAddressRegister(t *testing.T, p *m68k.Processor, r int, v uint32) {
-	if p.A[r] != v {
-		t.Errorf("expected address value 0x%08X in A%d, got 0x%08X", v, r, p.A[r])
-	}
-}
-
-func assertCCRRegister(t *testing.T, p *m68k.Processor, v uint32) {
-	if p.CCR != v {
-		t.Errorf("expected value 0x%X in CCR, got 0x%X", v, p.CCR)
-	}
-}
-
-func assertLong(t *testing.T, p *m68k.Processor, addr uint32, v uint32) {
-	b := make([]byte, 4)
-	_, err := p.M.Read(int(addr), b)
-	if err != nil {
-		t.Fatal(err)
-	}
-	actual := uint32(b[3]) | uint32(b[2])<<8 | uint32(b[1])<<16 | uint32(b[0])<<24
-	if actual != v {
-		t.Errorf("expected value 0x%08X at 0x%08X, got 0x%08X", v, addr, actual)
-		m68k.DumpState(os.Stdout, p)
-		m68k.Dump(os.Stdout, p.M)
-	}
-}
 
 func TestReset(t *testing.T) {
 	// init dirty processor
-	p := testProc(t)
+	p := m68ktest.NewProcessor(t)
 	for i := 0; i < 8; i++ {
 		p.A[i], p.D[i] = 0xFFFFFFFF, 0xFFFFFFFF
 	}
@@ -145,59 +46,59 @@ func TestReset(t *testing.T) {
 	}
 }
 
-// func TestAddi(t *testing.T) {
-// 	p := load("test-op-addi.h68")
-// 	assertRun(t, p)
-// 	assertDataRegister(t, p, 0, 0xFE)
-// 	assertDataRegister(t, p, 1, 0xFFFE)
-// 	assertDataRegister(t, p, 2, 0xFFFFFFFE)
-// 	assertDataRegister(t, p, 3, 0xFFFFFFFF)
-// }
+func TestAddi(t *testing.T) {
+	p := m68ktest.LoadFile(t, "testdata/test-op-addi.h68")
+	m68ktest.AssertRun(t, p)
+	m68ktest.AssertDataRegister(t, p, 0, 0xFE)
+	m68ktest.AssertDataRegister(t, p, 1, 0xFFFE)
+	m68ktest.AssertDataRegister(t, p, 2, 0xFFFFFFFE)
+	m68ktest.AssertDataRegister(t, p, 3, 0xFFFFFFFF)
+}
 
 func TestMoveL(t *testing.T) {
-	p := loadFile(t, "test-op-move-l.h68")
-	assertRun(t, p)
+	p := m68ktest.LoadFile(t, "testdata/test-op-move-l.h68")
+	m68ktest.AssertRun(t, p)
 
 	// source: data register
-	assertDataRegister(t, p, 1, 0x44304430)
-	assertAddressRegister(t, p, 1, 0x44304430)
-	assertLong(t, p, 0x2000, 0x44304430)
-	assertLong(t, p, 0x2004, 0x44314431)
-	assertLong(t, p, 0x2008, 0x44324432)
-	// assertLong(t, p, 0x200C, 0x44334433)
-	assertLong(t, p, 0x2010, 0x44344434)
-	assertLong(t, p, 0x12000, 0x44344434)
+	m68ktest.AssertDataRegister(t, p, 1, 0x44304430)
+	m68ktest.AssertAddressRegister(t, p, 1, 0x44304430)
+	m68ktest.AssertLong(t, p, 0x2000, 0x44304430)
+	m68ktest.AssertLong(t, p, 0x2004, 0x44314431)
+	m68ktest.AssertLong(t, p, 0x2008, 0x44324432)
+	// m68ktest.AssertLong(t, p, 0x200C, 0x44334433)
+	m68ktest.AssertLong(t, p, 0x2010, 0x44344434)
+	m68ktest.AssertLong(t, p, 0x12000, 0x44344434)
 
 	// source: address register
-	assertDataRegister(t, p, 2, 0x41304130)
-	assertAddressRegister(t, p, 2, 0x41304130)
-	assertLong(t, p, 0x3000, 0x41304130)
-	assertLong(t, p, 0x3004, 0x41314131)
-	assertLong(t, p, 0x3008, 0x41324132)
-	assertLong(t, p, 0x300C, 0x41334133)
-	assertLong(t, p, 0x13000, 0x41334133)
+	m68ktest.AssertDataRegister(t, p, 2, 0x41304130)
+	m68ktest.AssertAddressRegister(t, p, 2, 0x41304130)
+	m68ktest.AssertLong(t, p, 0x3000, 0x41304130)
+	m68ktest.AssertLong(t, p, 0x3004, 0x41314131)
+	m68ktest.AssertLong(t, p, 0x3008, 0x41324132)
+	m68ktest.AssertLong(t, p, 0x300C, 0x41334133)
+	m68ktest.AssertLong(t, p, 0x13000, 0x41334133)
 
 	// source: immediate
-	assertDataRegister(t, p, 3, 0x49304930)
-	assertAddressRegister(t, p, 3, 0x49304930)
-	assertLong(t, p, 0xF000, 0x49304930)
-	assertLong(t, p, 0xF004, 0x49314931)
-	assertLong(t, p, 0xF008, 0x49324932)
-	assertLong(t, p, 0xF00C, 0x49334933)
-	assertLong(t, p, 0x1F000, 0x49334933)
+	m68ktest.AssertDataRegister(t, p, 3, 0x49304930)
+	m68ktest.AssertAddressRegister(t, p, 3, 0x49304930)
+	m68ktest.AssertLong(t, p, 0xF000, 0x49304930)
+	m68ktest.AssertLong(t, p, 0xF004, 0x49314931)
+	m68ktest.AssertLong(t, p, 0xF008, 0x49324932)
+	m68ktest.AssertLong(t, p, 0xF00C, 0x49334933)
+	m68ktest.AssertLong(t, p, 0x1F000, 0x49334933)
 }
 
 func TestOri(t *testing.T) {
-	p := loadFile(t, "test-op-ori.h68")
+	p := m68ktest.LoadFile(t, "testdata/test-op-ori.h68")
 	p.D[2] = 0x22
 	p.D[3] = 0x2222
 	p.D[4] = 0x22222222
-	assertRun(t, p)
-	assertCCRRegister(t, p, 0x1F)
-	assertDataRegister(t, p, 1, 0xFFFFFFFF)
-	assertDataRegister(t, p, 2, 0x33)
-	assertDataRegister(t, p, 3, 0x3333)
-	assertDataRegister(t, p, 4, 0x33333333)
+	m68ktest.AssertRun(t, p)
+	m68ktest.AssertStatusRegister(t, p, 0x1F)
+	m68ktest.AssertDataRegister(t, p, 1, 0xFFFFFFFF)
+	m68ktest.AssertDataRegister(t, p, 2, 0x33)
+	m68ktest.AssertDataRegister(t, p, 3, 0x3333)
+	m68ktest.AssertDataRegister(t, p, 4, 0x33333333)
 }
 
 func TestAndi(t *testing.T) {
@@ -206,15 +107,15 @@ func TestAndi(t *testing.T) {
 		0x02, 0x41, 0x0F, 0xFF, // andi.w #$0FFF,D1
 		0x02, 0x81, 0x0F, 0x0F, 0xFF, 0xFF, // andi.l #$0F0FFFFF,D1
 	}
-	// p := loadFile(t, "test-op-andi.h68")
-	p := loadBytes(t, b)
+	// p := m68ktest.LoadFile(t, "testdata/test-op-andi.h68")
+	p := m68ktest.LoadBytes(t, b)
 	p.D[1] = 0x55555555
-	assertRun(t, p)
-	// assertCCRRegister(t, p, 0x1F)
-	assertDataRegister(t, p, 1, 0x05050505)
+	m68ktest.AssertRun(t, p)
+	// m68ktest.AssertStatusRegister(t, p, 0x1F)
+	m68ktest.AssertDataRegister(t, p, 1, 0x05050505)
 }
 
-var progAndiOri = loadBytes(nil, []byte{
+var progAndiOri = m68ktest.LoadBytes(nil, []byte{
 	0x00, 0x80, 0xFF, 0xFF, 0xFF, 0xFF, // ori.l	#$FFFFFFFF,D0
 	0x02, 0x80, 0x55, 0x55, 0x55, 0x55, // andi.l	#$55555555,D0
 	0x00, 0x80, 0xAA, 0xAA, 0xAA, 0xAA, // ori.l	#$AAAAAAAA,D0
---
+++ m68ktest/m68ktest.go
@@ -0,0 +1,118 @@
+/*
+Package m68ktest provides functions for testing Go code against the Motorola
+68000 chipset emulator.
+*/
+package m68ktest
+
+import (
+	"io"
+	"os"
+	"testing"
+
+	"github.com/cavaliercoder/go-m68k"
+	"github.com/cavaliercoder/go-m68k/srec"
+)
+
+type testLogWriter struct {
+	t *testing.T
+}
+
+func (t *testLogWriter) Write(p []byte) (n int, err error) {
+	if t.t == nil {
+		return
+	}
+	t.t.Logf("%s", p)
+	n = len(p)
+	return
+}
+
+// NewProcessor returns a 68000 processor that will log trace messages to the
+// given test context.
+func NewProcessor(t *testing.T) *m68k.Processor {
+	p := &m68k.Processor{
+		TraceWriter: &testLogWriter{t},
+	}
+	p.Reset()
+	return p
+}
+
+// LoadFile returns a new 680000 processor with the given S-Record format
+// program loaded into memory.
+func LoadFile(t *testing.T, path string) *m68k.Processor {
+	p := NewProcessor(t)
+	f, err := os.Open(path)
+	if err != nil {
+		panic(err)
+	}
+	defer f.Close()
+	recs, err := srec.Read(f)
+	if err != nil {
+		panic(err)
+	}
+	for _, rec := range recs {
+		if rec.IsData() {
+			if _, err := p.M.Write(rec.Address(), rec.Data()); err != nil {
+				panic(err)
+			}
+		}
+	}
+	return p
+}
+
+// LoadBytes returns a new 68000 processor with the given raw program data
+// loaded into memory.
+func LoadBytes(t *testing.T, b []byte) *m68k.Processor {
+	p := NewProcessor(t)
+	if _, err := p.M.Write(0x1000, b); err != nil {
+		panic(err)
+	}
+	return p
+}
+
+// AssertRun assert that the processor can run the program loaded into memory.
+func AssertRun(t *testing.T, p *m68k.Processor) {
+	err := p.Jump(0x1000)
+	if err != nil {
+		t.Errorf("error resetting program counter: %v", err)
+	}
+	err = p.Run()
+	if err == io.EOF {
+		return
+	}
+	if err != nil {
+		t.Errorf("error running program: %v", err)
+		m68k.Dump(p.TraceWriter, p.M)
+	}
+}
+
+func AssertDataRegister(t *testing.T, p *m68k.Processor, r int, v uint32) {
+	if p.D[r] != v {
+		t.Errorf("expected data value 0x%08X in D%d, got 0x%08X", v, r, p.D[r])
+	}
+}
+
+func AssertAddressRegister(t *testing.T, p *m68k.Processor, r int, v uint32) {
+	if p.A[r] != v {
+		t.Errorf("expected address value 0x%08X in A%d, got 0x%08X", v, r, p.A[r])
+	}
+}
+
+func AssertStatusRegister(t *testing.T, p *m68k.Processor, v uint32) {
+	if p.CCR != v {
+		t.Errorf("expected value 0x%X in CCR, got 0x%X", v, p.CCR)
+	}
+}
+
+func AssertLong(t *testing.T, p *m68k.Processor, addr uint32, v uint32) {
+	b := make([]byte, 4)
+	_, err := p.M.Read(int(addr), b)
+	if err != nil {
+		t.Fatal(err)
+	}
+	actual := uint32(b[3]) | uint32(b[2])<<8 | uint32(b[1])<<16 | uint32(b[0])<<24
+	if actual != v {
+		t.Errorf("expected value 0x%08X at 0x%08X, got 0x%08X", v, addr, actual)
+		m68k.DumpState(os.Stdout, p)
+		m68k.Dump(os.Stdout, p.M)
+	}
+}
