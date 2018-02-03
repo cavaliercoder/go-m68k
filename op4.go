5d1d1cbe746bfcce6ad93dc4f40221cf50903cf5
Ryan Armstrong <ryan@cavaliercoder.com>
2018-02-04 00:26:53 +0800

    Implemented JSR

--- op4.go
+++ op4.go
@@ -42,12 +42,16 @@ func op4A(c *Processor) (t *stepTrace) {
 // op4E routes for:
 // - TRAP (pg. 4-188)
 // - STOP (pg. 6-85)
+// - JSR (pg. 4-109)
 func op4E(c *Processor) (t *stepTrace) {
 	if c.op&0x00F0 == 0x0040 {
 		return opTrap(c)
 	}
 	if c.op == 0x4E72 {
 		return opStop(c)
+	}
+	if c.op&0x00C0 == 0x0080 {
+		return opJsr(c)
 	}
 	c.err = newOpcodeError(c.op)
 	return
@@ -119,6 +123,7 @@ func opTrap(c *Processor) (t *stepTrace) {
 	c.PC += 2
 	v += 32
 	if c.handlers[v] != nil {
+		// TODO: should trap handlers be executed inline?
 		c.err = c.handlers[v].Exception(c, int(v))
 	}
 	return
@@ -143,6 +148,57 @@ func opStop(c *Processor) (t *stepTrace) {
 	c.SR = uint32(n) & StatusMask
 	if c.SR&0x0700 == 0x0700 {
 		c.err = io.EOF // end program if interrupt mask is maximum
+	}
+	return
+}
+
+// opJsr implements JSR (4-109)
+func opJsr(c *Processor) (t *stepTrace) {
+	t = &stepTrace{
+		addr: c.PC,
+		op:   "jsr",
+		n:    1,
+		sz:   noSize,
+	}
+	c.PC += 2
+
+	mod := c.op & 0x38 >> 3
+	reg := c.op & 0x07
+	switch mod {
+	// TODO: implement remaining address modes for JSR
+	default:
+		c.err = newOpcodeError(c.op)
+		return
+
+	case 0x02: // address register
+		c.PC = c.A[reg]
+		t.src = fmt.Sprintf("(A%d)", reg)
+
+	case 0x07: // other
+		switch reg {
+		default:
+			c.err = newOpcodeError(c.op)
+			return
+
+		case 0x00: // absolute word
+			var n uint16
+			n, _, c.err = c.readImmWord()
+			c.PC = uint32(wordToInt32(n))
+			t.src = fmt.Sprintf("$%X", int32(n))
+
+		case 0x01: // absolute long
+			var n uint32
+			n, _, c.err = c.readImmLong()
+			c.PC = n
+			t.src = fmt.Sprintf("$%X", int32(n))
+
+		case 0x02: // program counter with displacement
+			var n uint16
+			n, _, c.err = c.readImmWord()
+			disp := wordToInt32(n)
+			c.PC = uint32(int32(c.PC) + disp - 2)
+			t.src = fmt.Sprintf("$%X(PC)", disp)
+		}
 	}
 	return
 }
